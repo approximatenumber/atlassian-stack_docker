07bd1d064a5723a02cb7ac237ce8f1838215d7ae
Мельников Максим Алексеевич <mamelnikov@spb.rpkb.ru>
2017-07-10 19:18:23 +0300

    total change

--- letsencrypt/first_generation.sh
+++
@@ -1,7 +0,0 @@
-EMAIL="me@domain.com"
-DOMAIN="domain.com"
-
-docker-compose run --rm letsencrypt \
-  letsencrypt certonly --webroot \
-  --email ${EMAIL} --agree-tos \
-  -w /var/www/letsencrypt -d ${DOMAIN}
